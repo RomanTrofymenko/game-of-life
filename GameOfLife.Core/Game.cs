3dfd518a5a9c35d2b7653d093ecf774896390e0d
Roman Trofimenko <rtrofimenko@emoneyadvisor.com>
2019-02-06 15:05:06 -0500

    Removed implementation. Added static structure generators to help visually check if the game works as supposed

--- GameOfLife.Core/Game.cs
+++ GameOfLife.Core/Game.cs
@@ -6,44 +6,7 @@ namespace GameOfLife
     {
         public bool[,] Progress(bool[,] initial)
         {
-            var result = new bool[initial.GetLength(0),initial.GetLength(1)];
-            for(var cellX = 0; cellX < initial.GetLength(0); cellX++)
-                for(var cellY = 0; cellY < initial.GetLength(1); cellY++)
-                {
-                    var neighborsAlive = GetNeighborsAlive(cellX, cellY, initial);
-                    result[cellX,cellY] = neighborsAlive == 3 || (neighborsAlive == 2 && initial[cellX, cellY]);
-                }
-            return result;
-        }
-
-        private int GetNeighborsAlive(int cellX, int cellY, bool[,] initial)
-        {
-            var result = 0;
-            for(var neighborX = cellX-1; neighborX < cellX+2; neighborX++)
-                for(var neighborY = cellY-1; neighborY < cellY+2; neighborY++)
-                {
-                    if(IsInBounds(neighborX, neighborY, initial)
-                    && IsNeighbor(neighborX, neighborY, cellX, cellY)
-                    && initial[neighborX, neighborY])
-                        result++;
-                }
-            return result;
-        }
-
-        private bool IsNeighbor(int neighborX, int neighborY, int cellX, int cellY)
-        {
-            var result = neighborX != cellX || neighborY != cellY;
-            return result;
-        }
-
-        private bool IsInBounds(int neighborX, int neighborY, bool[,] initial)
-        {
-            var width = initial.GetLength(0);
-            var height = initial.GetLength(1);
-            var yInBounds = neighborY >= 0 && neighborY < height;
-            bool xInBounds = neighborX >= 0 && neighborX < width;
-            var result = xInBounds && yInBounds;
-            return result;
+            return initial;
         }
     }
 }
--- GameOfLife.Test/GameTest.cs
+++ GameOfLife.Test/GameTest.cs
@@ -5,48 +5,5 @@ namespace GameOfLife.Test
     [TestClass]
     public class GameTest
     {
-        [TestMethod]
-        public void Progress_KillsCell_WhenItHasLessThanTwoNeighbors()
-        {
-            var input = new bool[,]{{true,false},{false,false}};
-            var subject = new Game();
-
-            var output = subject.Progress(input);
-
-            Assert.IsFalse(output[0,0]);
-        }
-
-        [TestMethod]
-        public void Progress_KillsCell_WhenItHasMoreThanThreeNeighbors()
-        {
-            var input = new bool[,]{{true,true,true},{true,false,true}};
-            var subject = new Game();
-
-            var output = subject.Progress(input);
-
-            Assert.IsFalse(output[0,1]);
-        }
-        
-        [TestMethod]
-        public void Progress_KeepsCell_WhenItHasTwoNeighbors()
-        {
-            var input = new bool[,]{{true,true},{true,false}};
-            var subject = new Game();
-
-            var output = subject.Progress(input);
-
-            Assert.IsTrue(output[0,0]);
-        }
-        
-        [TestMethod]
-        public void Progress_RevivesCell_WhenItHasThreeNeighbors()
-        {
-            var input = new bool[,]{{true,true},{true,false}};
-            var subject = new Game();
-
-            var output = subject.Progress(input);
-
-            Assert.IsTrue(output[1,1]);
-        }
     }
 }
