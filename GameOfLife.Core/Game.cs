4f07afe7b462e8593640ceb3a1d564ffbf94c197
Roman Trofimenko <rtrofimenko@emoneyadvisor.com>
2019-02-07 14:38:38 -0500

    results

--- GameOfLife.Core/Game.cs
+++ GameOfLife.Core/Game.cs
@@ -6,7 +6,52 @@ namespace GameOfLife
     {
         public bool[,] Progress(bool[,] initial)
         {
-            return initial;
-        }
-    }
+			var result = new bool[initial.GetLength(0), initial.GetLength(1)];
+
+			for(int i = 0; i < initial.GetLength(0); i++)
+			{
+				for(int j = 0; j < initial.GetLength(1); j++)
+				{
+					int count = 0;
+					var neighborValue = GetValueSafely(initial, i, j);
+					if (GetValueSafely(initial, i-1, j-1) == true)
+						count++;
+					if(GetValueSafely(initial, i-1, j) == true)
+						count++;
+					if(GetValueSafely(initial, i-1, j + 1) == true)
+						count++;
+					if (GetValueSafely(initial, i, j-1) == true)
+						count++;
+					if (GetValueSafely(initial, i, j + 1) == true)
+						count++;
+					if (GetValueSafely(initial, i + 1, j-1) == true)
+						count++;
+					if (GetValueSafely(initial, i + 1, j) == true)
+						count++;
+					if (GetValueSafely(initial, i + 1, j + 1) == true)
+						count++;
+
+					if(count >=2 && initial[i,j] == true)
+					{
+						result[i, j] = true;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private static bool GetValueSafely(bool[,] initial, int i, int j)
+		{
+			try
+			{
+				return initial[i, j];
+			}
+			catch (IndexOutOfRangeException)
+			{
+
+				return false;
+			}
+		}
+	}
 }
--- GameOfLife.Test/GameTest.cs
+++ GameOfLife.Test/GameTest.cs
@@ -5,5 +5,37 @@ namespace GameOfLife.Test
     [TestClass]
     public class GameTest
     {
-    }
+		[TestMethod]
+		public void Progress_KillsCells_WhithLessThanTwoNeighbors()
+		{
+			var initial = new bool[,] { { true, false }, { false, false } };
+
+			var next = new Game().Progress(initial);
+
+			Assert.IsFalse(next[0, 0]);
+		}
+
+		[TestMethod]
+		public void Progress_Lives_TwoLiveNeighborsLives()
+		{
+			var initial = new bool[,] { { true, false }, { true, true } };
+
+			var next = new Game().Progress(initial);
+
+			Assert.IsTrue(next[0, 0]);
+			Assert.IsTrue(next[1, 0]);
+			Assert.IsTrue(next[1, 1]);
+		}
+
+		[TestMethod]
+		public void Progress_KeepsDimensions()
+		{
+			var initial = new bool[3, 4];
+
+			var next = new Game().Progress(initial);
+
+			Assert.AreEqual(3, next.GetLength(0));
+			Assert.AreEqual(4, next.GetLength(1));
+		}
+	}
 }
